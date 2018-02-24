874dbe1c3efd70d0734cbf45a6e89dcd8750e133
Dai Jaafar <daijaafar@gmail.com>
2018-02-25 00:45:51 +0800

    done module Dynamically Duplicate Input Elements

--- Demo-Start/app/customers/customer.component.ts
+++ Demo-Start/app/customers/customer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validator, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
+import { FormGroup, FormBuilder, Validator, Validators, AbstractControl, ValidatorFn, FormArray } from '@angular/forms';
+import 'rxjs/add/operator/debounceTime';
 import { Customer } from './customer';
 
 function emailMatcher(c: AbstractControl) {
@@ -30,6 +31,16 @@ function ratingRange(min: number, max:number): ValidatorFn {
 export class CustomerComponent implements OnInit {
     customerForm: FormGroup;
     customer: Customer= new Customer();
+    emailMessage: string;
+
+    get addresses(): FormArray {
+        return <FormArray>this.customerForm.get('addresses');
+    }
+
+    private validationMessages = {
+        required: 'Please enter your email address.',
+        pattern: 'Please enter a valid email address.'
+    }
 
     constructor(private fb: FormBuilder) {
 
@@ -47,11 +58,31 @@ export class CustomerComponent implements OnInit {
             phone: '',
             notification:'email',
             rating: ['', ratingRange(1,5)],
-            sendCatalog:true
+            sendCatalog:true,
+            addresses: this.fb.array([ this.buildAddress() ])
         });
 
         this.customerForm.get('notification').valueChanges
             .subscribe(value => this.setNotification(value));
+
+        const emailControl = this.customerForm.get('emailGroup.email');
+        emailControl.valueChanges.debounceTime(1000).subscribe(value =>
+            this.setMessage(emailControl));
+    }
+
+    addAddresses(): void {
+        this.addresses.push(this.buildAddress());
+    }
+
+    buildAddress(): FormGroup {
+        return this.fb.group({
+            addressType: 'home',
+            street1: '',
+            street2: '',
+            city: '',
+            state: '',
+            zip: ''
+        })
     }
 
     populateTestData(): void {
@@ -61,6 +92,14 @@ export class CustomerComponent implements OnInit {
             email: 'jack@torchwood.com',
             sendCatalog: false
         });
+    }
+
+    setMessage(c: AbstractControl): void {
+        this.emailMessage = '';
+        if ((c.touched || c.dirty) && c.errors) {
+            this.emailMessage = Object.keys(c.errors).map(key =>
+                this.validationMessages[key]).join(' ');
+        }
     }
 
     save() {
